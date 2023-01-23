184fd51269d9d1ee7368feb6dee22fee9778b8e7
Luke Diebold <luke@ldiebold.com>
2023-01-24 07:29:46 +1000

    initial commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue'
+
+const todo = ref('')
+
+const todos = ref([])
+</script>
+
+<template>
+  <q-page class="flex flex-center">
+    <q-input
+      v-model="todo"
+      label="My Input"
+      outlined
+      color="blue-9"
+    />
+
+    <q-btn
+      label="submit"
+      color="green"
+      unelevated
+      :loading="loading"
+      @click="() => {
+        todos.push({
+          done: false,
+          title: todo
+        })
+        todo = ''
+      }"
+    />
+
+    <pre>{{ todos }}</pre>
+  </q-page>
+</template>
